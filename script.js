17ea474f6e85b30dc63694bd88eb1039df85639b
AlexWang1017 <149715013+AlexWang1017@users.noreply.github.com>
2023-12-10 18:06:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-document.getElementById('nextNavButton').addEventListener('click', function() {
-  var currentNavItem = document.querySelector('.nav-menu a:hover');
+document.querySelector('.nextNavButton').addEventListener('click', function() {
+  var currentNavItem = document.querySelector('.nav-menu a.active');
   var nextNavItem = currentNavItem.parentElement.nextElementSibling;
+
   if (nextNavItem) {
       window.location.href = nextNavItem.querySelector('a').getAttribute('href');
   }
-})
+});
