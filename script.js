2e34c721f9f9f0ab92e710495e1ace1d9dcfc6b4
lCSeric <133724439+lCSeric@users.noreply.github.com>
2023-12-10 21:25:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.querySelector('.nextNavButton').addEventListener('click', function() {
-  var currentNavItem = document.querySelector('.nav-menu a.active');
+document.getElementById('nextNavButton').addEventListener('click', function() {
+  var currentNavItem = document.querySelector('.nav-menu a:hover');
   var nextNavItem = currentNavItem.parentElement.nextElementSibling;
-
   if (nextNavItem) {
       window.location.href = nextNavItem.querySelector('a').getAttribute('href');
   }
-});
+})
+
